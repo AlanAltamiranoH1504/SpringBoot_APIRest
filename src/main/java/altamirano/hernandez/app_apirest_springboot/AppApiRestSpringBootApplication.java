5bb2868dd6e3d081f69d5f0b837500f9747f9001
Alan Altamirano Hernandez <altamiranohernandezalan@gmail.com>
2025-05-06 17:53:44 -0600

    initial commit

---
+++ src/main/java/altamirano/hernandez/app_apirest_springboot/AppApiRestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package altamirano.hernandez.app_apirest_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApiRestSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApiRestSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/altamirano/hernandez/app_apirest_springboot/AppApiRestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package altamirano.hernandez.app_apirest_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApiRestSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
